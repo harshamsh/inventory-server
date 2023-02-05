62e2d3fe1401782bdadb19377122f3603aa9f109
Sri Harsha Musunuru <57175686+harshamsh@users.noreply.github.com>
2023-02-05 20:54:30 +0000

    Code cleaned & documented

--- server/app.js
+++ server/app.js
@@ -4,9 +4,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 
+// Load environment variables from .env file
 require("dotenv").config();
 
+// Middleware for parsing the incoming requests as JSON
 app.use(bodyParser.json());
+
+// Middleware for handling CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
@@ -17,6 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,21 +33,33 @@ db.once("open", function () {
   console.log("Connected to MongoDB successfully");
 });
 
+// Define the schema for the "Item" collection
 const itemSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true, // Require the "name" field to be filled
+  },
   description: String,
-  quantity: Number,
+  quantity: {
+    type: Number,
+    default: 1, // Default value for "quantity" if not specified
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, // Default value for "createdAt" to the current time
+  },
 });
 
+// Create a model based on the "Item" schema
 const Item = mongoose.model("Item", itemSchema);
 
-app.use(express.json());
-
+// Get all items
 app.get("/api/items", async (req, res) => {
   const items = await Item.find();
   res.send(items);
 });
 
+// Get a single item by ID
 app.get("/api/items/:id", (req, res) => {
   const id = req.params.id;
   Item.findById(id, (err, item) => {
@@ -53,12 +70,14 @@ app.get("/api/items/:id", (req, res) => {
   });
 });
 
+// Create a new item
 app.post("/api/items", async (req, res) => {
   const item = new Item(req.body);
   await item.save();
   res.send(item);
 });
 
+// Update an existing item by ID
 app.put("/api/items/:id", async (req, res) => {
   const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -66,12 +85,14 @@ app.put("/api/items/:id", async (req, res) => {
   res.send(item);
 });
 
+// Delete an existing item by ID
 app.delete("/api/items/:id", async (req, res) => {
   const item = await Item.findByIdAndDelete(req.params.id);
   res.send(item);
 });
 
-app.get("/api/items/search/?", async (req, res) => {
+// Find items by Name
+app.get("/api/items/search", async (req, res) => {
   console.log("Received searchTerm: ", req.query.searchTerm);
   const searchTerm = req.query.searchTerm;
   try {
@@ -83,6 +104,7 @@ app.get("/api/items/search/?", async (req, res) => {
   }
 });
 
+// App Listening at PORT
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
